a938ec9426d28310cf9d39ba1fd74d01f8f53b30
Nicklas Knell <nicklas.knell@gmail.com>
2022-06-13 22:35:22 +0200

    Load clients after maximizing

--- main.js
+++ main.js
@@ -1,40 +1,49 @@
 const { app, screen, BrowserWindow, BrowserView } = require('electron')
 
-const createWindow = () => {
-    const primaryDisplay = screen.getPrimaryDisplay();
-    const { width, height } = primaryDisplay.workAreaSize;
+let sizedProperly = false;
+let window;
 
-    const win = new BrowserWindow({
-        width,
-        height
-    });
-
+function setResizedTrue() {
+    const { width, height } = window.getBounds();
     const quarterWidth = Math.floor(width / 2);
     const quarterHeight = Math.floor(height / 2);
+    
+    console.log("Width", width, "Height", height)
+    if(sizedProperly === false) {
+        const view1 = new BrowserView();
+        window.addBrowserView(view1);
+        view1.setBounds({ x: 0, y: 0, width: quarterWidth, height: quarterHeight })
+        view1.webContents.loadURL('http://universe.flyff.com/play')
+        
+        const view2 = new BrowserView();
+        window.addBrowserView(view2);
+        view2.setBounds({ x: quarterWidth, y: 0, width: quarterWidth, height: quarterHeight })
+        view2.webContents.loadURL('http://universe.flyff.com/play')
+        
+        const view3 = new BrowserView();
+        window.addBrowserView(view3);
+        view3.setBounds({ x: 0, y: quarterHeight, width: quarterWidth, height: quarterHeight })
+        view3.webContents.loadURL('http://universe.flyff.com/play')
+        
+        const view4 = new BrowserView();
+        window.addBrowserView(view4);
+        view4.setBounds({ x: quarterWidth, y: quarterHeight, width: quarterWidth, height: quarterHeight })
+        view4.webContents.loadURL('http://universe.flyff.com/play')
 
-    const view1 = new BrowserView()
-    win.addBrowserView(view1)
-    view1.setAutoResize({ horizontal: true, vertical: true, width: true, height: true })
-    view1.setBounds({ x: 0, y: 0, width: quarterWidth, height: quarterHeight })
-    view1.webContents.loadURL('http://universe.flyff.com/play')
+        sizedProperly = true;
+    }
+}
 
-    const view2 = new BrowserView()
-    win.addBrowserView(view2)
-    view2.setAutoResize({ horizontal: true, vertical: true, width: true, height: true })
-    view2.setBounds({ x: quarterWidth, y: 0, width: quarterWidth, height: quarterHeight })
-    view2.webContents.loadURL('http://universe.flyff.com/play')
+const createWindow = () => {
+    window = new BrowserWindow({});
 
-    const view3 = new BrowserView()
-    win.addBrowserView(view3)
-    view3.setAutoResize({ horizontal: true, vertical: true, width: true, height: true })
-    view3.setBounds({ x: 0, y: quarterHeight, width: quarterWidth, height: quarterHeight })
-    view3.webContents.loadURL('http://universe.flyff.com/play')
-
-    const view4 = new BrowserView()
-    win.addBrowserView(view4)
-    view4.setAutoResize({ horizontal: true, vertical: true, width: true, height: true })
-    view4.setBounds({ x: quarterWidth, y: quarterHeight, width: quarterWidth, height: quarterHeight })
-    view4.webContents.loadURL('http://universe.flyff.com/play')
+    window.loadFile("main.html");
+    
+    window.on('resize', function () {
+        if(window.isMaximized()) {
+            setResizedTrue();   
+        }
+    });
 }
 
 app.whenReady().then(() => {
