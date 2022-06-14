07b1d2fbeec5097deac6addc4e23a064962c320e
Nicklas Knell <nicklas.knell@gmail.com>
2022-06-14 20:25:59 +0200

    Remove menu to prevent content from being cutoff

--- main.js
+++ main.js
@@ -7,24 +7,25 @@ function setResizedTrue() {
     const { width, height } = window.getBounds();
     const quarterWidth = Math.floor(width / 2);
     const quarterHeight = Math.floor(height / 2);
-    
+
     console.log("Width", width, "Height", height)
-    if(sizedProperly === false) {
+    if (sizedProperly === false) {
+        window.setMenu(null);
         const view1 = new BrowserView();
         window.addBrowserView(view1);
         view1.setBounds({ x: 0, y: 0, width: quarterWidth, height: quarterHeight })
         view1.webContents.loadURL('http://universe.flyff.com/play')
-        
+
         const view2 = new BrowserView();
         window.addBrowserView(view2);
         view2.setBounds({ x: quarterWidth, y: 0, width: quarterWidth, height: quarterHeight })
         view2.webContents.loadURL('http://universe.flyff.com/play')
-        
+
         const view3 = new BrowserView();
         window.addBrowserView(view3);
         view3.setBounds({ x: 0, y: quarterHeight, width: quarterWidth, height: quarterHeight })
         view3.webContents.loadURL('http://universe.flyff.com/play')
-        
+
         const view4 = new BrowserView();
         window.addBrowserView(view4);
         view4.setBounds({ x: quarterWidth, y: quarterHeight, width: quarterWidth, height: quarterHeight })
@@ -38,10 +39,10 @@ const createWindow = () => {
     window = new BrowserWindow({});
 
     window.loadFile("main.html");
-    
+
     window.on('resize', function () {
-        if(window.isMaximized()) {
-            setResizedTrue();   
+        if (window.isMaximized()) {
+            setResizedTrue();
         }
     });
 }
